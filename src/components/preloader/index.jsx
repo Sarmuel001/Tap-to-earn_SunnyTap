8f614ded13ee54dfc0b08d776c0d6ec9321f6f56
sarmuel001 <adebayosunday51@gmail.com>
2025-02-14 16:44:38 +0100

    Feb 14 updates

--- src/components/preloader/index.jsx
+++ src/components/preloader/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./preloader.css";
+import preAI from './preAI.jpg'
 
 const Preloader = () => {
   return (
     <div className="preloader"  >
     <div className="image-container">
-       <img src="/src/assets/mine.jpg" alt="Loading" /> 
+       <img src={preAI} alt="Loading" /> 
     </div>
     <div className="loader"></div>
   </div>
